91b4e2108f8a71a44bb7462f8b9845cb4accbe8f
NiclasDev63 <niclas.gregor20@gmail.com>
2022-02-23 17:55:39 +0100

    .

--- main.py
+++ main.py
@@ -36,6 +36,42 @@ class Blue(Wall):
         self.color("blue")
 
 # The MAZE
+# grid = [
+# "1111111111111111111111111111111111",
+# "1000000000000000000000000000000001",
+# "1010111111111111100111111111110101",
+# "1010100001000000100001000000010101",
+# "1011101111111110111001011111010101",
+# "1000001000000010001001000100010101",
+# "1011101111111011101001110111011101",
+# "1011101111111011101001110111011101",
+# "1010101000001010001001010001000101",
+# "1010111011111010111001011101110101",
+# "1010000010000010100000000101000101",
+# "1010111110101110111111111101011101",
+# "1010100000101000000000000001010001",
+# "1010101011101111111111011111010101",
+# "1010101010100000000001010001010101",
+# "1010101010111110111111010111011101",
+# "1010101010100010100000000100000101",
+# "1010101010101110111111110111110101",
+# "1000101010101000000000010000010101",
+# "1011101110101111111001110111110101",
+# "1010000000100000000001000100010101",
+# "1010111110111011111101110101110101",
+# "1010100010001010000100010101000101",
+# "1011101110111010100111011101110101",
+# "1010001000101010100001010000010101",
+# "1010111011101010111101110111010101",
+# "1010100010100010000100000101000101",
+# "1010101110111011100111011101111101",
+# "1000100000101000100101010000000001",
+# "1000100000101000100101010000000001",
+# "1011101110101111111101010111110101",
+# "1010001010000000000000010100010101",
+# "1011111s1111111111111101110111111e",
+# "0000000000000000000000000000000100"]
+
 grid = [
 "1111111111111111111111111111111111",
 "1000000000000000000000000000000001",
@@ -49,26 +85,26 @@ grid = [
 "1010111011111010111001011101110101",
 "1010000010000010100000000101000101",
 "1010111110101110111111111101011101",
-"1010100000101000000000000001010001",
+"1010100000101001111111100001010001",
 "1010101011101111111111011111010101",
 "1010101010100000000001010001010101",
 "1010101010111110111111010111011101",
 "1010101010100010100000000100000101",
 "1010101010101110111111110111110101",
-"1000101010101000000000010000010101",
+"1000101010101001111100010000010101",
 "1011101110101111111001110111110101",
 "1010000000100000000001000100010101",
 "1010111110111011111101110101110101",
-"1010100010001010000100010101000101",
+"1010111110001010000100010101000101",
 "1011101110111010100111011101110101",
 "1010001000101010100001010000010101",
 "1010111011101010111101110111010101",
 "1010100010100010000100000101000101",
 "1010101110111011100111011101111101",
 "1000100000101000100101010000000001",
-"1000100000101000100101010000000001",
+"1000100000101110100101010000000001",
 "1011101110101111111101010111110101",
-"1010001010000000000000010100010101",
+"1010001010000000011100010100010101",
 "1011111s1111111111111101110111111e",
 "0000000000000000000000000000000100"]
 
@@ -105,13 +141,12 @@ def paint_maze(grid):
 
             if char == "s":
                 paint_blob(x, y, blue)
+
     return start_x, start_y, end_x, end_y
 
 
 # implements DFS
 def _depthFirstSearch(visited, x, y, pred, end_x, end_y):
-
-    pred[start_x][start_y] = [-1, -1]
 
     if x == end_x and y == end_y:
         shortestPath(pred)
@@ -155,8 +190,6 @@ def breadthFirstSreach(start_x, start_y, end_x, end_y):
 
     queue = deque([[start_x, start_y]])
 
-    pred[start_x][start_y] = [-1, -1]
-
 
     while queue:
         x, y = queue.popleft()
@@ -190,6 +223,97 @@ def breadthFirstSreach(start_x, start_y, end_x, end_y):
                 if not visited[x][y - 1]:
                     pred[x][y - 1] = [x, y]
 
+# implements Dijkstra`s Algorithm
+def _minEntfernung(dist, sptSet):
+    
+       
+        min = float("inf")
+
+      
+        for x in range(len(grid)):
+            for y in range(len(grid)):
+
+                if dist[x][y] < min and sptSet[x][y] == False:
+
+                    min = dist[x][y]
+
+                    min_index = x, y
+
+                    #print(min_index)
+
+        return min_index
+    
+
+def dijkstra(start_x, start_y, end_x, end_y):
+
+    dist = [[float("inf") for _ in range(len(grid[0]))]for _ in range(len(grid))]
+
+    sptSet = [[False for _ in range(len(grid[0]))]for _ in range(len(grid))]
+
+    pred = [[[-1, -1] for _ in range(len(grid[0]))]for _ in range(len(grid))]
+
+    unvisited = [[[-1, -1] for _ in range(len(grid[0]))]for _ in range(len(grid))]
+
+    dist[start_x][start_y] = 0
+    
+    while unvisited:
+        
+        x, y = _minEntfernung(dist, sptSet)
+
+        if x == end_x and y == end_y:
+            shortesPathDijkstra(dist, pred)
+
+        if [x, y] != [start_x, start_y]:
+                paint_blob(x, y, red)
+        
+        sptSet[x][y] = True
+
+        if x + 1 < len(grid[0]) and grid[y][x + 1] != "0":
+                if not sptSet[x + 1][y]:
+                    pred[x + 1][y] = [x, y]
+                    if dist[x + 1][y] > dist[x][y] + 1:
+                         dist[x + 1][y] = dist[x][y] + 1
+ 
+        if  x - 1 >= 0 and grid[y][x - 1] != "0":
+            if not sptSet[x - 1][y]:
+                pred[x - 1][y] = [x, y]
+                if dist[x - 1][y] > dist[x][y] + 1:
+                         dist[x - 1][y] = dist[x][y] + 1
+
+        if y + 1 < len(grid) and grid[y + 1][x] != "0":
+            if not sptSet[x][y + 1]:
+                pred[x][y + 1] = [x, y]
+                if dist[x][y + 1] > dist[x][y] + 1:
+                         dist[x][y + 1] = dist[x][y] + 1
+
+        if y - 1 >= 0 and grid[y - 1][x] != "0":
+            if not sptSet[x][y - 1]:
+                pred[x][y - 1] = [x, y]
+                if dist[x][y - 1] > dist[x][y] + 1:
+                         dist[x][y - 1] = dist[x][y] + 1
+
+
+        
+        unvisited[x][y].pop()
+
+# paint the shortest path for Dijkstra`s Algorithm
+def shortesPathDijkstra(dist, pred):
+    min = float("inf")
+    path = []
+    
+    x = end_x
+    y = end_y
+    while(pred[x][y] != [-1, -1]):
+        if dist[x][y] < min:
+            path.append(pred[x][y])
+            x, y = pred[x][y]
+
+    for i in range(len(path)-2, -1, -1):
+        x, y = path[i]
+        paint_blob(x, y, yellow)
+    window.exitonclick()
+
+
 # paint the shortest path
 def shortestPath(pred):
     path = []
@@ -212,7 +336,7 @@ if __name__ == "__main__":
     print("##        CHOOSE AN ALGORITHM        ##")
     print("##                                   ##")
     print("#######################################")
-    choose_algorithm = int(input("Press 1 for BFS and 2 for DFS: ")) #You can choose betwenn two Algorithms
+    choose_algorithm = int(input("Press 1 for BFS / 2 for DFS and 3 for Dijkstra`s Algorithm: ")) #You can choose betwenn two Algorithms
 
 
     window = turtle.Screen()
@@ -230,6 +354,11 @@ if __name__ == "__main__":
     start_x, start_y, end_x, end_y = paint_maze(grid)
 
     if choose_algorithm == 1:
+        print("Breadth First Search is running...")
         breadthFirstSreach(start_x, start_y, end_x, end_y)
-    else:
+    elif choose_algorithm == 2:
+        print("Depth First Search is running...")
         depthFirstSearch(start_x, start_y, end_x, end_y)
+    else:
+        print("Dijkstra`s Algorithm is running...")
+        dijkstra(start_x, start_y, end_x, end_y)
